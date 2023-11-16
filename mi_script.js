039e4cd63ba8ad00e80bb735d52529f90daf0172
nkmelndz <nkmelndz@gmail.com>
2023-11-15 22:26:42 -0500

    slider

--- mi_script.js
+++ mi_script.js
@@ -7,27 +7,109 @@ $(document).ready(function () {
       $(".postulantes").removeClass("showpostulantes");
     }
   );
+  $(".postulantes").hover(
+    function () {
+      $(".postulantes").addClass("showpostulantes");
+    },
+    function () {
+      $(".postulantes").removeClass("showpostulantes");
+    }
+  );
 });
 
 $(document).ready(function () {
-    $(".postulantes").hover(
-        function () {
-          $(".postulantes").addClass("showpostulantes");
-        },
-        function () {
-          $(".postulantes").removeClass("showpostulantes");
-        }
-      );
+  $("#estudiantes").hover(
+    function () {
+      $(".estudiantes").addClass("showestudiantes");
+    },
+    function () {
+      $(".estudiantes").removeClass("showestudiantes");
+    }
+  );
+  $(".estudiantes").hover(
+    function () {
+      $(".estudiantes").addClass("showestudiantes");
+    },
+    function () {
+      $(".estudiantes").removeClass("showestudiantes");
+    }
+  );
 });
 
 
-  $(document).ready(function () {
-    $("#estudiantes").hover(
-      function () {
-        $(".estudiantes").addClass("showestudiantes");
-      },
-      function () {
-        $(".estudiantes").removeClass("showestudiantes");
-      }
-    );
-  });
+$(document).ready(function () {
+  $("#egresados").hover(
+    function () {
+      $(".egresados").addClass("showegresados");
+    },
+    function () {
+      $(".egresados").removeClass("showegresados");
+    }
+  );
+  $(".egresados").hover(
+    function () {
+      $(".egresados").addClass("showegresados");
+    },
+    function () {
+      $(".egresados").removeClass("showegresados");
+    }
+  );
+});
+
+$(document).ready(function () {
+  $("#empleabilidad").hover(
+    function () {
+      $(".empleabilidad").addClass("showempleabilidad");
+    },
+    function () {
+      $(".empleabilidad").removeClass("showempleabilidad");
+    }
+  );
+  $(".empleabilidad").hover(
+    function () {
+      $(".empleabilidad").addClass("showempleabilidad");
+    },
+    function () {
+      $(".empleabilidad").removeClass("showempleabilidad");
+    }
+  );
+});
+
+$(document).ready(function () {
+  $("#intranet").hover(
+    function () {
+      $(".intranet").addClass("showintranet");
+    },
+    function () {
+      $(".intranet").removeClass("showintranet");
+    }
+  );
+  $(".intranet").hover(
+    function () {
+      $(".intranet").addClass("showintranet");
+    },
+    function () {
+      $(".intranet").removeClass("showintranet");
+    }
+  );
+});
+
+$(document).ready(function () {
+  $("#pregrado").hover(
+    function () {
+      $(".pregrado").addClass("showpregrado");
+    },
+    function () {
+      $(".pregrado").removeClass("showpregrado");
+    }
+  );
+  $(".pregrado").hover(
+    function () {
+      $(".pregrado").addClass("showpregrado");
+    },
+    function () {
+      $(".pregrado").removeClass("showpregrado");
+    }
+  );
+});
+
